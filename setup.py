17adcf260f4e1501efa0f1a87e1a6017ef0726e8
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2020-12-17 13:53:52 +0500

    adding setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NMT-PSL',
+    version='0.0.1',
+    description='Pakistan sign language text translation',
+    url='https://github.com/Shahraizz/NMT-PSL.git',
+    author='Shahraiz Anwer',
+    author_email='shehraizbhatti11@gmail.com',
+    license='unlicense',
+    packages=['NMT-PSL'],
+    zip_safe=False
+)
