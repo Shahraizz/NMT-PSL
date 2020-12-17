5551497167026502816dc8e4045a79877fed71e0
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2020-12-17 19:41:57 +0500

    #

--- NMT_PSL/Tokenizers.py
+++ NMT_PSL/Tokenizers.py
@@ -2,6 +2,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow_datasets as tfds
+from transformers import AutoTokenizer
 
 def TFTokenizer(
     train_data,test_data, inp_lang, targ_lang, 
@@ -87,3 +88,62 @@ def TFSubTokenizer(
     targ_tensor_test = pad_sequences(test_targ, padding=pad)
     
     return tokenizer, (inp_tensor_train, targ_tensor_train, inp_tensor_test, targ_tensor_test)
+
+
+def bertTokenizer(train_data,test_data, inp_lang, targ_lang, pad='post'):
+    
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    
+    train_inp = np.array(train_data[inp_lang])
+    train_targ = np.array(train_data[targ_lang])
+    test_inp = np.array(test_data[inp_lang])
+    test_targ = np.array(test_data[targ_lang])
+    
+    #max_length_inp = train_data[inp_lang].map(lambda x: len(x.split(' '))).max()
+    #max_length_targ = train_data[targ_lang].map(lambda x: len(x.split(' '))).max()
+    #print('Max_length of input sequence: {}'.format(max_length_inp))
+    #print('Max_length of target sequence: {}'.format(max_length_targ))
+    
+    max_length_inp = None
+    max_length_targ = None
+    padd = True
+    
+    train_inp_encoded = tokenizer(
+        list(train_inp),
+        max_length= max_length_inp,
+        padding= padd, 
+        return_tensors= 'tf', truncation=True)['input_ids']
+
+    train_targ_encoded = tokenizer(
+        list(train_targ),
+        max_length= max_length_targ,
+        padding= padd, 
+        return_tensors= 'tf', truncation=True)['input_ids']
+
+    test_inp_encoded = tokenizer(
+        list(test_inp),
+        max_length= max_length_inp,
+        padding= padd, 
+        return_tensors= 'tf', truncation=True)['input_ids']
+
+    test_targ_encoded = tokenizer(
+        list(test_targ),
+        max_length= max_length_targ,
+        padding= padd, 
+        return_tensors= 'tf', truncation=True)['input_ids']
+    
+    #max_length_inp = max(list(map(lambda x: len(x), train_inp_encoded)))
+    #max_length_targ = max(list(map(lambda x: len(x), train_targ_encoded)))
+    #max_length_inp_test = max(list(map(lambda x: len(x), test_inp_encoded)))
+    #max_length_targ_test = max(list(map(lambda x: len(x), test_targ_encoded)))
+
+    #print('Max_length of input sequence train: {}'.format(max_length_inp))
+    #print('Max_length of target sequence train: {}'.format(max_length_targ))
+    #print('Max_length of input sequence test: {}'.format(max_length_inp_test))
+    #print('Max_length of target sequence test: {}'.format(max_length_targ_test))
+    
+    return tokenizer,(train_inp_encoded, 
+                      train_targ_encoded, 
+                      test_inp_encoded, 
+                      test_targ_encoded
+                     )
---
+++ NMT_PSL/utils.py
@@ -0,0 +1,31 @@
+
+
+
+def create_tf_dataset(
+    train_pair,dev_pair,test_pair,batch_size_train,
+    batch_size_dev,batch_size_test,buffer_size=0, verbose=0):
+
+    ## Dataset for train
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_pair).shuffle(buffer_size)
+    train_dataset = train_dataset.batch(batch_size_train, drop_remainder=True)
+
+    ## Dataset for validation
+    val_dataset = tf.data.Dataset.from_tensor_slices(dev_pair)
+    val_dataset = val_dataset.batch(batch_size_dev, drop_remainder=True)
+
+    ## Dataset for test 
+    test_dataset = tf.data.Dataset.from_tensor_slices(test_pair)
+    test_dataset = test_dataset.batch(batch_size_test, drop_remainder=True)
+    
+    if verbose:
+        print('Number of train Batches: {}'.format(
+            len(list(train_dataset.as_numpy_iterator()))
+        ))
+        print('Number of val Batches: {}'.format(
+            len(list(val_dataset.as_numpy_iterator()))
+        ))
+        print('Number of test Batches: {}'.format(
+            len(list(test_dataset.as_numpy_iterator()))
+        ))
+    
+    return train_dataset, val_dataset, test_dataset
