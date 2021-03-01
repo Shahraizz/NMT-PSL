7247fb87a0ad5649101bb98d748b795510c90f15
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2021-03-01 19:33:05 +0500

    #

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NMT_PSL',
-    version='0.0.12',
+    version='0.0.13',
     description='Pakistan sign language text translation',
     url='https://github.com/Shahraizz/NMT-PSL.git',
     author='Shahraiz Anwer',
