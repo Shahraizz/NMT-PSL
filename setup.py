5c3bf88f7e6d5c53c08700481e79bd50d3b9b724
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2021-03-01 15:16:02 +0500

    Adding Encoder decoder and attention

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NMT_PSL',
-    version='0.0.6',
+    version='0.0.7',
     description='Pakistan sign language text translation',
     url='https://github.com/Shahraizz/NMT-PSL.git',
     author='Shahraiz Anwer',
