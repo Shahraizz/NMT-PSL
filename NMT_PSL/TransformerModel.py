87d4c671d982fc9a859ebbf66bec1474be03ca89
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2021-03-05 18:37:12 +0500

    #

--- NMT_PSL/TransformerModel.py
+++ NMT_PSL/TransformerModel.py
@@ -280,6 +280,48 @@ class Decoder(tf.keras.layers.Layer):
     # x.shape == (batch_size, target_seq_len, d_model)
     return x, attention_weights
 
+################################################################
+
+
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+
+    # add extra dimensions to add the padding
+    # to the attention logits.
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len) 
+
+
+######
+
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+
+
+
+
+def create_masks(inp, tar, bert):
+    # Encoder padding mask
+    if bert:
+        enc_padding_mask = bert_padding_mask(inp)
+    else:
+        enc_padding_mask = create_padding_mask(inp)
+
+    # Used in the 2nd attention block in the decoder.
+    # This padding mask is used to mask the encoder outputs.
+    dec_padding_mask = create_padding_mask(inp)
+
+    # Used in the 1st attention block in the decoder.
+    # It is used to pad and mask future tokens in the input received by 
+    # the decoder.
+    look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+    dec_target_padding_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+
+    return enc_padding_mask, combined_mask, dec_padding_mask
+
+
 
 
 class Transformer(tf.keras.Model):
