1830732e43e8329d6883a4fc680413c5919e90a7
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2021-03-05 22:22:47 +0500

    #

--- NMT_PSL/TransformerTraining.py
+++ NMT_PSL/TransformerTraining.py
@@ -1,15 +1,137 @@
-from .TransformerModel import Transformer
-from .TransformerModel import create_padding_mask
-from .TransformerModel import create_look_ahead_mask
-from .TransformerModel import create_masks
+from NMT_PSL.TransformerModel import Transformer
+from NMT_PSL.TransformerModel import create_padding_mask
+from NMT_PSL.TransformerModel import create_look_ahead_mask
+from NMT_PSL.TransformerModel import create_masks
+
+from NMT_PSL import Embeddings
+from NMT_PSL import utils
+from NMT_PSL.Tokenizers import psl_tokenizer
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
-
+from configparser import ConfigParser
 import numpy as np
+import pandas as pd
+
+import time
+
+#############
+
+config = ConfigParser()
+config.read('config.ini')
+#############
+
+
+CONFIG = { 
+    "preprocessing":{
+        "tokenizer": 'bpe',
+        "input_lanuage": "English",
+        "targ_language": "PSL",
+        "val_rand_state": 37,
+        "val_size": 3000,
+        "batch_size_train": 64,
+        "batch_size_test": 500,
+        "batch_size_dev": 1000,
+    },
+    "transformer":{
+        "num_layers": 6,
+        "dff": 2048,
+        "num_heads": 4,
+        "bert_enc": False,
+        "d_model": 300,
+        "tok_size": 42,
+        "dropout_rate": 0.1,
+        "embedding": 'bpe',
+        "train_emb": False
+    }
+}
+
+TOKENIZER = 'bpe' ## [tf, tfsub, bpe, bert]
+input_language = "English"
+targ_language = "PSL"
+
+random_state = 37
+val_size = 3000
+
+BATCH_SIZE_TRAIN = 64
+BATCH_SIZE_TEST = 500
+BATCH_SIZE_VAL = 500
+
+EPOCHS = 15
+
+# transformer base parameters
+num_layers = 6
+dff = 2048
+num_heads = 4
+bert_enc = False
+
+d_model = 300
+tok_size = 42
+train_emb = False
+
+embedding = 'bpe'
+
+
+dropout_rate = 0.1
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+
+        self.warmup_steps = warmup_steps
+    
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+    
+    
+    
+##############
+
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
 
 
 
-def inference_step(inp, targ):
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
+
+
+
+##########
+
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='train_accuracy')
+
+val_loss = tf.keras.metrics.Mean(name='val_loss')
+val_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='val_accuracy')
+
+learning_rate = CustomSchedule(d_model)
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, 
+                                     epsilon=1e-9)
+
+
+
+def inference_step(inp, targ, start_tok, transformer):
     
     max_len = targ.shape[1]
     encoder_input = inp
@@ -46,7 +168,6 @@ def inference_step(inp, targ):
     val_accuracy(targ_real, sen_pred)
     
     return output[:,1:], attention_weights
-    #return tf.math.argmax(sen_pred, -1), attention_weights
 
 #######
 
@@ -73,7 +194,7 @@ def inference(data):
 
 
 @tf.function()
-def train_step(inp, tar):
+def train_step(inp, tar, transformer, bert=False):
   tar_inp = tar[:, :-1]
   tar_real = tar[:, 1:]
   
@@ -93,3 +214,126 @@ def train_step(inp, tar):
   train_loss(loss)
   train_accuracy(tar_real, predictions)
 
+
+
+
+def loadDataset(path, test_size, random_state):
+
+    #!wget -O ./Osama.xlsx  "https://docs.google.com/spreadsheets/d/1dXEZYseoUVZ1jXUTNyoHNH5ujE4bxdC3IligxPh65uE/export?gid=0&format=xlsx"
+    
+    df = pd.read_excel(path, usecols=['English', 'PSL']).dropna()
+    
+    df = shuffle(df,random_state=random_state).reset_index(drop=True)
+
+    train_data = df.iloc[:-test_size].reset_index(drop=True)
+
+    test_data = df.iloc[-test_size:].reset_index(drop=True)
+    
+    return train_data, test_data
+
+
+def train_model(path):
+
+    df_train, df_test = loadDataset(path, 2000, 69)
+
+    lang, vocab_size, start_tok, end_tok, (inp_tensor_train, targ_tensor_train, inp_tensor_test, targ_tensor_test) = psl_tokenizer(
+        TOKENIZER, df_train, df_test, input_language, targ_language, pad="post"
+    )
+
+    val_size = utils.set_dev_size(val_size,inp_tensor_train.shape[0])
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        inp_tensor_train,
+        targ_tensor_train,
+        test_size=val_size,
+        random_state=random_state
+    )
+
+    input_vocab_size = vocab_size
+    target_vocab_size = vocab_size
+
+    train_dataset, val_dataset, test_dataset = util.create_tf_dataset(
+        (X_train, y_train),(X_val, y_val),(inp_tensor_test, targ_tensor_test),
+        BATCH_SIZE_TRAIN, BATCH_SIZE_VAL, BATCH_SIZE_TEST, verbose=True
+    )
+
+    misses = []
+
+    if embedding is 'bert':
+        embedding_dim = 768
+        #embedding_matrix = loadBert()
+    elif embedding is 'glove':
+        embedding_matrix, misses, embedding_index = Embeddings.loadGlove(d_model, vocab_size, lang, tok_size)
+    elif embedding is 'fasttext':
+        embedding_matrix, misses = Embeddings.loadFasttext(200000, lang, vocab_size, subword=False)
+    elif embedding is 'bpe':
+        embedding_matrix = Embeddings.loadBpeEmb(lang)
+        d_model = 100
+        
+    elif embedding is 'uniform':
+        embedding_matrix = 'uniform'
+
+
+    transformer = Transformer(num_layers, d_model, num_heads, dff,
+                          input_vocab_size, target_vocab_size, 
+                          pe_input=input_vocab_size, 
+                          pe_target=target_vocab_size,
+                          rate=dropout_rate,
+                          emd_matrix=embedding_matrix,
+                          train_emb = train_emb,
+                          bert = bert_enc
+                         )
+
+    checkpoint_path = "./checkpoints/train"
+
+    ckpt = tf.train.Checkpoint(transformer=transformer,
+                           optimizer=optimizer)
+
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=1)
+
+    history = {'train_loss':[],
+           'val_loss':[],
+           'train_acc':[],
+           'val_acc':[]
+          }
+
+
+    for epoch in range(EPOCHS):
+        start = time.time()
+    
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        
+
+    
+        # inp -> english, tar -> psl
+        for (batch, (inp, tar)) in enumerate(train_dataset):
+            train_step(inp, tar)
+        
+            if batch % 100 == 0:
+                print ('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                    epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+        
+        #if (epoch + 1) % 5 == 0:
+        #    ckpt_save_path = ckpt_manager.save()
+        #    print ('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+        #                                                         ckpt_save_path))
+        print ('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
+                                                            train_loss.result(), 
+                                                            train_accuracy.result()))
+        
+        inference(val_dataset) # calculate validation loss
+        print ('Epoch {} val Loss {:.4f} val Accuracy {:.4f}'.format(epoch + 1,
+                                                                    val_loss.result(), 
+                                                                    val_accuracy.result()))
+
+        print ('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
+        
+        history['train_loss'].append(train_loss.result())
+        history['train_acc'].append(train_accuracy.result())
+        history['val_loss'].append(val_loss.result())
+        history['val_acc'].append(val_accuracy.result())
+
+
+
+  
--- NMT_PSL/utils.py
+++ NMT_PSL/utils.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 
 def create_tf_dataset(
     train_pair,dev_pair,test_pair,batch_size_train,
-    batch_size_dev,batch_size_test,buffer_size=0, verbose=False):
+    batch_size_dev,batch_size_test, verbose=False):
 
     ## Dataset for train
-    train_dataset = tf.data.Dataset.from_tensor_slices(train_pair).shuffle(buffer_size)
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_pair).shuffle(len(train_pair[0]))
     train_dataset = train_dataset.batch(batch_size_train, drop_remainder=True)
 
     ## Dataset for validation
