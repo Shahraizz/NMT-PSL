ed7d64e8950329b76dbe12aeb644f4f92e4859f4
Shahraiz Anwer <shehraizbhatti11@gmail.com>
2021-03-05 17:41:53 +0500

    #

--- NMT_PSL/TransformerModel.py
+++ NMT_PSL/TransformerModel.py
@@ -23,20 +23,10 @@ def positional_encoding(position, d_model):
 
 
 
-def create_padding_mask(seq):
-    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
-
-    # add extra dimensions to add the padding
-    # to the attention logits.
-    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len) 
 
 def bert_padding_mask(seq):
     return tf.cast(tf.math.equal(seq, 0), tf.float32)
 
-
-def create_look_ahead_mask(size):
-    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-    return mask  # (seq_len, seq_len)
 
 
 def scaled_dot_product_attention(q, k, v, mask):
